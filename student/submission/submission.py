903e373759ab87637df5d40a6000dd06a3352161
Gregory Bell <gjbell@trihardstudios.com>
2023-02-11 15:40:13 -0700

    Wrote test autograder

--- student/submission/submission.py
+++ student/submission/submission.py
@@ -1,7 +1,27 @@
-import time
-i: str = str(input("NUMBER> "))
+print("1. Addition\n2. Subtraction\n3. Multiplcation 4. Integer Division\n5. Division")
 
-print(f"OUTPUT {int(i, 2)}")
-print(f"OUTPUT 1000")
 
-# time.sleep(9)
+mode = input("MODE> ")
+
+number1 = float(input("NUMBER 1> "))
+number2 = float(input("NUMBER 2> "))
+
+result = 0;
+
+if mode == "1":
+    result = number1 + number2
+
+elif mode == "2":
+    result = number1 - number2
+
+elif mode == "3":
+    result = number1 * number2
+
+elif mode == "4":
+    result = number1 // number2
+
+elif mode == "5":
+    result = f"{number1 / number2:.02f}"
+
+
+print(f"OUTPUT {result}")
