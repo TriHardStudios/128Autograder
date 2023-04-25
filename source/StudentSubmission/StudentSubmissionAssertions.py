ed4830c101aba6bc8c73b4a655eaecb5b6f29df3
Gregory Bell <gjbell@trihardstudios.com>
2023-04-24 22:32:23 -0600

    Started work on file IO support

--- source/StudentSubmission/StudentSubmissionAssertions.py
+++ source/StudentSubmission/StudentSubmissionAssertions.py
@@ -1,3 +1,6 @@
+import os
+from io import StringIO
+
 from .StudentSubmission import StudentSubmission
 
 
@@ -7,37 +10,60 @@ class StudentSubmissionAssertions:
             raise AssertionError(_studentSubmission.getValidationError())
 
 
-class StudentSubmissionStdIOAssertions(StudentSubmissionAssertions):
+    class StdIOAssertions:
 
-    def assertSubmissionExecution(self, _studentSubmission: StudentSubmission,
-                                  _stdin: list[str], _stdout: list[str],
-                                  _timeout: int):
+        def assertSubmissionExecution(self, _studentSubmission: StudentSubmission,
+                                      _stdin: list[str], _stdout: list[str],
+                                      _timeout: int):
 
-        status, stdout = _studentSubmission.runMainModule(_stdin, _timeout)
+            status, stdout = _studentSubmission.runMainModule(_stdin, _timeout)
 
-        if not status:
-            stdout = [error for error in stdout if 'output' not in error.lower()]
-            msg: str = "Failed to execute student submission."
+            if not status:
+                stdout = [error for error in stdout if 'output' not in error.lower()]
+                msg: str = "Failed to execute student submission."
 
-            if stdout:
-                msg += " Error(s):\n"
-                for error in stdout:
-                    msg += error + "\n"
+                if stdout:
+                    msg += " Error(s):\n"
+                    for error in stdout:
+                        msg += error + "\n"
 
-            raise AssertionError(msg)
+                raise AssertionError(msg)
 
-        _stdout.extend(stdout)
+            _stdout.extend(stdout)
 
-    def assertCorrectNumberOfOutputLines(self, expected: list[str], actual: list[str]):
-        if len(actual) == 0:
-            raise AssertionError("No OUTPUT lines found. Check OUTPUT formatting.")
+        def assertCorrectNumberOfOutputLines(self, expected: list[str], actual: list[str]):
+            if len(actual) == 0:
+                raise AssertionError("No OUTPUT lines found. Check OUTPUT formatting.")
 
-        if len(actual) > len(expected):
-            raise AssertionError(f"Too many OUTPUT lines. Check OUTPUT formatting.\n"
-                                 f"Expected number of lines: {len(expected)}\n"
-                                 f"Actual number of lines  : {len(actual)}")
+            if len(actual) > len(expected):
+                raise AssertionError(f"Too many OUTPUT lines. Check OUTPUT formatting.\n"
+                                     f"Expected number of lines: {len(expected)}\n"
+                                     f"Actual number of lines  : {len(actual)}")
 
-        if len(actual) < len(expected):
-            raise AssertionError(f"Too few OUTPUT lines. Check OUTPUT formatting.\n"
-                                 f"Expected number of lines: {len(expected)}\n"
-                                 f"Actual number of lines  : {len(actual)}")
+            if len(actual) < len(expected):
+                raise AssertionError(f"Too few OUTPUT lines. Check OUTPUT formatting.\n"
+                                     f"Expected number of lines: {len(expected)}\n"
+                                     f"Actual number of lines  : {len(actual)}")
+
+
+    class FileIOAssertions:
+        @staticmethod
+        def readFromFile(_fileName: str) -> StringIO:
+            if not os.path.exists(_fileName):
+                raise AssertionError(f"File '{_fileName}' does not exist")
+
+            fileContents = open(_fileName, 'r').readline()
+
+            return StringIO(fileContents)
+
+        @staticmethod
+        def readFromArray(_stdin: list[str]) -> StringIO:
+            combinedInput = "".join([el + '\n' for el in _stdin])
+
+            return StringIO(combinedInput)
+
+        def assertSubmissionExecution(self, _studentSubmission: StudentSubmission, _input: StringIO, _expected: StringIO, _actual: StringIO):
+            pass
+
+        def assertFileOutput(self, expectedOutput: StringIO, actualOutput: StringIO):
+            pass
