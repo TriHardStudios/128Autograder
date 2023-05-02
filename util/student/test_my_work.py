31fcca62e6d61ee8f4784b8d544c0a8fe9cd2a95
Gregory Bell <gjbell@trihardstudios.com>
2023-05-02 09:38:25 -0600

    Finished inital version of student autograder

--- util/student/test_my_work.py
+++ util/student/test_my_work.py
@@ -2,46 +2,85 @@ import sys
 import os
 import subprocess
 
-def verfiyRequiredPackages() -> bool:
-    errorOccured: bool = False
+PACKAGE_ERROR: str = "Required Package Error"
+SUBMISSION_ERROR: str = "Student Submission Error"
+RED_COLOR: str = u"\u001b[31m"
+RESET_COLOR: str = u"\u001b[0m"
+
+
+def printErrorMessage(_errorType: str, _errorText: str) -> None:
+    """
+    This function prints out a validation error message as they occur.
+    The error type is colored red when it is printed
+    the format used is `[<error_type>] <error_text>`
+    :param _errorType: The error type
+    :param _errorText: the text for the error
+    :return:
+    """
+    print(f"[{RED_COLOR}{_errorText}{RESET_COLOR}]: {_errorText}")
+
+
+def verifyRequiredPackages() -> bool:
+    """
+    This function verifies that all the required packages needed for the actual autograder are installed
+    :return: true if they are false if not.
+    """
+    errorOccurred: bool = False
 
     try:
         import BetterPyUnitFormat
     except ModuleNotFoundError:
-        print("[Required Package Error] BetterPyUnitFormat was not found. Please ensure that you have run 'pip install Better-PyUnit-Format'")
-        errorOccured = True
+        printErrorMessage(PACKAGE_ERROR,
+                          "BetterPyUnitFormat was not found. "
+                          "Please ensure that you have run 'pip install Better-PyUnit-Format'")
+        errorOccurred = True
 
     try:
         import gradescope_utils
     except ModuleNotFoundError:
-        print("[Required Package Error] Gradescope Utils was not found. Please ensure that you have run 'pip install gradescope-utils'")
-        errorOccured = True
+        printErrorMessage(PACKAGE_ERROR,
+                          "Gradescope Utils was not found. "
+                          "Please ensure that you have run 'pip install gradescope-utils'")
+        errorOccurred = True
 
-    return not errorOccured
+    return not errorOccurred
+
 
 def verifyStudentWorkPresent(_submissionDirectory: str) -> bool:
+    """
+    This function verifies that the student has work in the submission directory
+    :param _submissionDirectory: the directory that the student did their work in.
+    :return: true if the student has work present
+    """
+
     if not os.path.exists(_submissionDirectory):
-        print(f"[Student Submission Error] Failed to locate student work in {_submissionDirectory}")
+        printErrorMessage(SUBMISSION_ERROR, f"Failed to locate student work in {_submissionDirectory}")
         return False
 
     if not os.path.isdir(_submissionDirectory):
-        print(f"[Student Submission Error] submissionDirectoy is not a directory.")
+        printErrorMessage(SUBMISSION_ERROR, f"{_submissionDirectory} is not a directory.")
         return False
 
     if len(os.listdir(_submissionDirectory)) < 2:
-        print(f"[Student Submission Error] no valid files found in submission directory. Found {os.listdir(_submissionDirectory)}")
+        printErrorMessage(SUBMISSION_ERROR,
+                          f"No valid files found in submission directory. "
+                          f"Found {os.listdir(_submissionDirectory)}")
         return False
 
     return True
 
 
 if __name__ == "__main__":
-    submissionDirectory = "student_work"
+    submissionDirectory = "student_work/"
 
     if len(sys.argv) == 2:
         submissionDirectory = sys.argv[1]
 
-    if not verfiyRequiredPackages():
+    # need to make sure to that we have a / at the end of the path
+    if submissionDirectory[:-1] != '/':
+        submissionDirectory += "/"
+
+    if not verifyRequiredPackages():
         sys.exit(1)
 
     if not verifyStudentWorkPresent(submissionDirectory):
@@ -49,10 +88,6 @@ if __name__ == "__main__":
 
     command: list[str] = ["python3", "run.py", "--unit-test-only", submissionDirectory]
 
-
     with subprocess.Popen(command, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True) as p:
         for line in p.stdout:
             print(line, end="")
-
-
-
