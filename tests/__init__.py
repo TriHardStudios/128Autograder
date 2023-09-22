b80fb6bb68c3cf14f10059c00f61771eff21366a
Gregory Bell <gjbell@trihardstudios.com>
2023-09-22 00:27:43 -0600

    Fix unit tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,6 +8,6 @@ import sys
 
 project_path = os.getcwd()
 
-source_path = os.path.join(project_path, "source", "util")
+source_path = os.path.join(project_path, "source")
 
 sys.path.append(source_path)
