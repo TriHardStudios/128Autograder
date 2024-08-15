06259b1bfc2c688a058a9447af7e0c541e302b7c
Gregory Bell <trihard.studios@outlook.com>
2024-08-15 15:36:40 -0600

    Start writing PL test

---
+++ tests/e2e/test_run_with_prairie_learn/student_tests/test.py
@@ -0,0 +1,37 @@
+import unittest
+from autograder_utils.Decorators import Weight
+
+from Executors.Executor import Executor
+from Executors.Environment import ExecutionEnvironmentBuilder, getResults
+from StudentSubmissionImpl.Python.PythonSubmission import PythonSubmission
+from utils.config.Config import AutograderConfigurationProvider
+from StudentSubmissionImpl.Python.Runners import PythonRunnerBuilder
+
+
+class HelloWorld(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.autograderConfig = AutograderConfigurationProvider.get()
+
+        cls.studentSubmission = PythonSubmission() \
+            .setSubmissionRoot(cls.autograderConfig.config.student_submission_directory) \
+            .enableLooseMainMatching() \
+            .load() \
+            .build()
+
+    def setUp(self) -> None:
+        self.environmentBuilder = ExecutionEnvironmentBuilder()
+
+    @Weight(10)
+    def testCode(self):
+        environment = self.environmentBuilder.build()
+
+        runner = PythonRunnerBuilder(self.studentSubmission) \
+            .setEntrypoint(module=True) \
+            .build()
+
+        Executor.execute(environment, runner)
+
+        actualOutput = getResults(environment).stdout
+
+        self.assertEqual("Hello World", actualOutput[0])
