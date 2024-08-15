86ccbcb40c582cc5093d7a3f9128d0a2508932f0
Gregory Bell <trihard.studios@outlook.com>
2024-08-15 15:46:45 -0600

    filter out non status lines

--- tests/e2e/test_run_test_my_work/verify.py
+++ tests/e2e/test_run_test_my_work/verify.py
@@ -9,5 +9,7 @@ with open("results/output.txt", "r") as r:
 
 # simply make sure that we had no errors or failures reported
 for line in student_results:
+    if "[" not in line:
+        continue
     assert "error" not in line.lower()
     assert "fail" not in line.lower()
