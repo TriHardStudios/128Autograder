c104d8cff4e6a79494857803278c9d6efa0aff77
Gregory Bell <trihard.studios@outlook.com>
2024-09-16 15:01:05 -0600

    Fix failing Test My Work change (#106)
    * make pipeline run on pull request as well :)
    
    * update entry point to be in the same working directory
    
    * use built version of autograder for e2e test
    
    ---------
    
    Co-authored-by: Gregory Bell <gjbell@trihardstudios.com>

--- tests/e2e/test_run_test_my_work/entrypoint.sh
+++ tests/e2e/test_run_test_my_work/entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-python /app/source/utils/student/test_my_work.py /app/submission > /app/results/output.txt
+# build autograder
+
+mkdir -p /app/build
+
+python run.py --build -o /app/build || exit 1
+
+# go to student directory
+cd /app/build/generation/student || exit 1
+
+# run the boi in a more realistic way
+# we are just overriding the submission diretory to point to the already correct submission
+python test_my_work.py /app/submission > /app/results/output.txt
